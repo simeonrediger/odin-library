51278da2d2efdcebf55f94bff0362b00fb30644c
Simeon Rediger <simeonrediger@gmail.com>
2025-09-17 02:21:33 -0400

    Refactor display object literal as class

--- script.js
+++ script.js
@@ -13,103 +13,156 @@ class Book {
     }
 }
 
-const display = {
-    tbody: document.querySelector('#book-table-data'),
+class Display {
+    static #rowTemplate;
 
-    addRow(book) {
-        const row = this.formatBookAsRow(book);
-        this.tbody.append(row);
-    },
-
-    getRow(book) {
-        return document.querySelector(`[data-id='${book.id}']`);
-    },
-
-    toggleStatus(book) {
-        this.getRow(book).querySelector('.status').textContent = (
-            getStatusDisplayFormat(book.isRead)
-        );
-    },
-
-    removeRow(bookId) {
-        this.tbody.querySelector(`[data-id='${bookId}']`)?.remove();
-    },
-
-    formatBookAsRow(book) {
+    static {
         const template = document.querySelector('#book-row-template');
-        const rowTemplate = template.content.querySelector('tr');
-        const row = rowTemplate.cloneNode(true);
+        this.#rowTemplate = template.content.querySelector('tr');
+    }
+
+    static formatBookAsRow(book) {
+        const row = this.#rowTemplate.cloneNode(true);
         row.dataset.id = book.id;
         row.querySelector('.title').textContent = book.title;
         row.querySelector('.author').textContent = book.author;
         row.querySelector('.page-count').textContent = book.pageCount;
         row.querySelector('.status').textContent = (
-            getStatusDisplayFormat(book.isRead)
+            this.getStatusDisplayFormat(book.isRead)
         );
 
         return row;
-    },
-};
-
-function onAddBook(event) {
-    const formData = new FormData(event.target);
-    const book = new Book(
-        formData.get('title'),
-        formData.get('author'),
-        Number(formData.get('pageCount')),
-        formData.has('isRead'),
-    );
-
-    library.push(book);
-    display.addRow(book);
-}
-
-function onCancelBookEntry() {
-    document.querySelector('#new-book-modal').close();
-}
-
-function onBookTableClick(event) {
-    const button = event.target.closest('button');
-
-    if (!button) {
-        return;
     }
 
-    const toggleStatusButtonClicked = (
-        button.classList.contains('toggle-status-button')
-    );
-
-    const deleteButtonClicked = (
-        button.classList.contains('delete-button')
-    );
-
-    if (toggleStatusButtonClicked) {
-        onToggleStatus(event);
-    } else if (deleteButtonClicked) {
-        onDeleteBook(event);
-    } else {
-        return;
+    static getStatusDisplayFormat(isRead) {
+        return isRead ? 'Read' : 'Unread';
     }
-}
 
-function onDeleteBook(event) {
-    const bookId = getBookIdFromActionButton(event.target);
-    const bookIndex = getBookIndexFromId(bookId);
-    library.splice(bookIndex, 1);
-    display.removeRow(bookId);
-}
+    constructor(
+        library,
+        newBookModalSelector,
+        newBookFormSelector,
+        cancelNewBookEntryButtonSelector,
+        newBookButtonSelector,
+        tbodySelector,
+    ) {
+        this.library = library;
+        this.newBookModal = document.querySelector(newBookModalSelector);
+        this.newBookForm = document.querySelector(newBookFormSelector);
+        this.cancelNewBookEntryButton = document.querySelector(
+            cancelNewBookEntryButtonSelector
+        );
+        this.newBookButton = document.querySelector(newBookButtonSelector);
+        this.tbody = document.querySelector(tbodySelector);
 
-function onToggleStatus(event) {
-    const bookId = getBookIdFromActionButton(event.target);
-    const bookIndex = getBookIndexFromId(bookId);
-    const book = library[bookIndex];
-    book.toggleStatus();
-    display.toggleStatus(book);
-}
+        this.#bindEvents();
+    }
 
-function getBookIdFromActionButton(actionButton) {
-    const bookRow = actionButton.parentNode.parentNode;
-    return bookRow.dataset.id;
+    addRow(book) {
+        const row = this.constructor.formatBookAsRow(book);
+        this.tbody.append(row);
+    }
+
+    getRow(book) {
+        return document.querySelector(`[data-id='${book.id}']`);
+    }
+
+    toggleStatus(book) {
+        this.getRow(book).querySelector('.status').textContent = (
+            this.constructor.getStatusDisplayFormat(book.isRead)
+        );
+    }
+
+    removeRow(bookId) {
+        this.tbody.querySelector(`[data-id='${bookId}']`)?.remove();
+    }
+
+    getBookIdFromActionButton(actionButton) {
+        const bookRow = actionButton.parentNode.parentNode;
+        return bookRow.dataset.id;
+    }
+
+    #onAddBook(event) {
+        const formData = new FormData(event.target);
+        const book = new Book(
+            formData.get('title'),
+            formData.get('author'),
+            Number(formData.get('pageCount')),
+            formData.has('isRead'),
+        );
+
+        this.library.push(book);
+        this.addRow(book);
+    }
+
+    #onCancelBookEntry() {
+        this.newBookModal.close();
+    }
+
+    #onBookTableClick(event) {
+        const button = event.target.closest('button');
+
+        if (!button) {
+            return;
+        }
+
+        const toggleStatusButtonClicked = (
+            button.classList.contains('toggle-status-button')
+        );
+
+        const deleteButtonClicked = (
+            button.classList.contains('delete-button')
+        );
+
+        if (toggleStatusButtonClicked) {
+            this.#onToggleStatus(event);
+        } else if (deleteButtonClicked) {
+            this.#onDeleteBook(event);
+        } else {
+            return;
+        }
+    }
+
+    #onDeleteBook(event) {
+        const bookId = getBookIdFromActionButton(event.target);
+        const bookIndex = getBookIndexFromId(bookId);
+        this.library.splice(bookIndex, 1);
+        this.removeRow(bookId);
+    }
+
+    #onToggleStatus(event) {
+        const bookId = getBookIdFromActionButton(event.target);
+        const bookIndex = getBookIndexFromId(bookId);
+        const book = this.library[bookIndex];
+        book.toggleStatus();
+        this.toggleStatus(book);
+    }
+
+    #bindEvents() {
+
+        this.newBookButton.addEventListener(
+            'click',
+            () => {
+                document.querySelector('#new-book-form').reset();
+                document.querySelector('#new-book-modal').showModal();
+            },
+        );
+
+        this.cancelNewBookEntryButton.addEventListener(
+            'click',
+            this.#onCancelBookEntry,
+        );
+
+        this.newBookForm.addEventListener(
+            'submit',
+            this.#onAddBook,
+        );
+
+        this.tbody.addEventListener(
+            'click',
+            this.#onBookTableClick,
+        );
+    }
 }
 
 function getBookIndexFromId(id) {
@@ -120,35 +173,6 @@ function getBookIndexFromId(id) {
     }
 
     return bookIndex;
-}
-
-function getStatusDisplayFormat(isRead) {
-    return isRead ? 'Read' : 'Unread';
-}
-
-function bindEvents() {
-    document.querySelector('#new-book-button').addEventListener(
-        'click',
-        () => {
-            document.querySelector('#new-book-form').reset();
-            document.querySelector('#new-book-modal').showModal();
-        },
-    );
-
-    document.querySelector('#cancel-new-book-entry-button').addEventListener(
-        'click',
-        onCancelBookEntry,
-    );
-
-    document.querySelector('#new-book-form').addEventListener(
-        'submit',
-        onAddBook,
-    );
-
-    document.querySelector('#book-table-data').addEventListener(
-        'click',
-        onBookTableClick,
-    );
 }
 
 const demo = {
@@ -169,6 +193,14 @@ const demo = {
     ],
 };
 
-const library = [];
-bindEvents();
+// const library = [];
+const display = new Display(
+    [],
+    '#new-book-modal',
+    '#new-book-form',
+    '#cancel-new-book-entry-button',
+    '#new-book-button',
+    '#book-table-data',
+);
+
 demo.run();
